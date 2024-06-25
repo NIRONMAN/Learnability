171630c3350097f75313b680309761fc326ce869
Niranjan <108979457+NIRONMAN@users.noreply.github.com>
2024-06-25 19:52:34 +0530

    added all the files including env

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -15,7 +15,7 @@ const Signup = (props: Props) => {
       className="bg-cover bg-center w-screen h-screen flex justify-center items-center"
       style={{ backgroundImage: 'url("https://images.unsplash.com/photo-1457276587196-a9d53d84c58b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8bGlicmFyeSUyMGJhY2tncm91bmR8ZW58MHx8MHx8fDA%3D")' }}
     >
-      <div className="bg-teal-500 w-[50%] h-[60%] flex rounded-lg relative ">
+      <div className="bg-teal-500 w-[50%] h-[60%] flex rounded-lg ">
         <div
           className={`bg-white w-1/2 h-[120%] rounded-lg relative bottom-10 left-10 transform transition-transform duration-500 ${
             moveCard ? 'translate-x-60' : ''
