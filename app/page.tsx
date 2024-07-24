a4b5d4cea0c63e05fc3e66eefd2012a33373e049
Niranjan <108979457+NIRONMAN@users.noreply.github.com>
2024-07-24 19:49:09 +0530

    Adding the landing page to root

--- app/page.tsx
+++ app/page.tsx
@@ -1,58 +1,88 @@
-"use client";
-import { Button, Input } from 'antd';
-import { useRouter } from 'next/navigation';
-import React, { useState } from 'react';
-import ytdl from 'ytdl-core';
+// "use client";
+// import { Button, Input } from 'antd';
+// import { useRouter } from 'next/navigation';
+// import React, { useState } from 'react';
+// import ytdl from 'ytdl-core';
 
-// Explicitly declaring the type of props as an empty object
-type Props = {};
+// // Explicitly declaring the type of props as an empty object
+// type Props = {};
 
-const Page: React.FC<Props> = (props) => {
+// const Page: React.FC<Props> = (props) => {
  
-  const router = useRouter();
-  const [url,setUrl]=useState<string>("")
+//   const router = useRouter();
+//   const [url,setUrl]=useState<string>("")
 
-  const handleRevisionNavigation = () => {
+//   const handleRevisionNavigation = () => {
 
-    const pathname = "/revising";
-    const query = { sessionId: "new-Chat" }; // Ensure sessionId is a string
-    const queryString = new URLSearchParams(query).toString();
-    router.push(`${pathname}?${queryString}`);
-  };
-  async function handleClick(){
+//     const pathname = "/revising";
+//     const query = { sessionId: "new-Chat" }; // Ensure sessionId is a string
+//     const queryString = new URLSearchParams(query).toString();
+//     router.push(`${pathname}?${queryString}`);
+//   };
+//   async function handleClick(){
     
-    const pathName="/youtube";
-    const query ={url}
-    const queryString= new URLSearchParams(query).toString()
-    router.push(`${pathName}?${queryString}`)
-}
-  return (
-    <div className=' bg-slate-900 h-screen flex justify-center items-center flex-col gap-5'>
-      <Button 
-        onClick={() => {
-          router.push("/learning");
-        }}
-        color='blue'
-      >
-        Go to Learning
-      </Button>
-      <Button 
-        color='yellow' 
-        onClick={handleRevisionNavigation}
-      >
-        Go to Revision
-      </Button>
-      <div className='p-4 w-full flex flex-col justify-center items-center'>
-        <h2 className='text-white p-2'>Enter the URL of a Youtube video</h2>
-      <Input 
-      className='w-2/3' 
-      onChange={(e)=>{
-        setUrl(e.target.value)
-      }}
-      ></Input>
-      </div>
+//     const pathName="/youtube";
+//     const query ={url}
+//     const queryString= new URLSearchParams(query).toString()
+//     router.push(`${pathName}?${queryString}`)
+// }
+//   return (
+//     <div className=' bg-slate-900 h-screen flex justify-center items-center flex-col gap-5'>
+//       <Button 
+//         onClick={() => {
+//           router.push("/learning");
+//         }}
+//         color='blue'
+//       >
+//         Go to Learning
+//       </Button>
+//       <Button 
+//         color='yellow' 
+//         onClick={handleRevisionNavigation}
+//       >
+//         Go to Revision
+//       </Button>
+//       <div className='p-4 w-full flex flex-col justify-center items-center'>
+//         <h2 className='text-white p-2'>Enter the URL of a Youtube video</h2>
+//       <Input 
+//       className='w-2/3' 
+//       onChange={(e)=>{
+//         setUrl(e.target.value)
+//       }}
+//       ></Input>
+//       </div>
       
-      <Button onClick={handleClick}>click to Submit</Button>
+//       <Button onClick={handleClick}>click to Submit</Button>
+//     </div>
+//   );
+// };
+
+// export default Page;
+
+"use client";
+import React from "react";
+import ParticlsBackground from "./Components/ParticlsBackground";
+import Header from "./Components/Header";
+import MainContent from "./Components/MainContent";
+
+
+
+
+const Page = () => {
+
+   return (
+    <div className="min-h-screen  flex flex-col">
+      <ParticlsBackground/>
+      <Header />
+      <div className="flex-grow flex flex-col items-center justify-center">
+        <MainContent />
+      </div>
+      <footer className="w-full p-4 flex justify-center items-center bg-gray-700">
+        <p className="text-white text-center">© 2024 Learnability. All rights reserved.</p>
+      </footer>
+
+        
+      
     </div>
   );
 };
