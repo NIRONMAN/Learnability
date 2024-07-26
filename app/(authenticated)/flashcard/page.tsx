98e679b5fb6c4f10a0b52c14daeb3d5d4fe41770
Yash <yash.magare1001@gmail.com>
2024-07-26 23:56:17 +0530

    adding functional flashcard

--- app/(authenticated)/flashcard/page.tsx
+++ app/(authenticated)/flashcard/page.tsx
@@ -1,81 +1,109 @@
 "use client";
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Spin } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '@/app/GlobalRedux/store';
+import { useRouter, useSearchParams } from 'next/navigation';
+import { getSession, sessionProps } from '@/utils/functions';
+import FlipCard from '@/app/Components/flashcardComponents/FlipCard';
 
+type Props = {};
 
+const Page = (props: Props) => {
+  const router = useRouter();
+  const { user, isLoadingCookie } = useSelector((state: RootState) => state.auth);
+  const searchParams = useSearchParams();
+  const sessionId = searchParams.get('sessionId');
+  const userId = user?.userId;
+  const dispatch = useDispatch();
+  const [response, setResponse] = useState<{ question: string, answer: string }[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentCardIndex, setCurrentCardIndex] = useState(0);
+  const [isCardExiting, setIsCardExiting] = useState(false);
+  const [currIndex,setIndex] = useState(0)
+  const [queReset,setQuesReset] = useState(false)
 
-import axios from 'axios'
-import { Spin } from 'antd'
-import { useDispatch, useSelector } from 'react-redux'
-import { RootState } from '@/app/GlobalRedux/store'
-import { useRouter, useSearchParams } from 'next/navigation'
-import { getSession, sessionProps } from '@/utils/functions'
-
-    type Props = {}
-    
-    const page = (props: Props) => {
-      const router = useRouter();
-      const { user, isLoadingCookie } = useSelector((state: RootState) => state.auth);
-      const searchParams = useSearchParams();
-      const sessionId = searchParams.get('sessionId');
-      const userId = user?.userId;
-      const dispatch=useDispatch()
-        const [response,setResponse]=useState("")
-        const [isLoading,setIsLoading]=useState(true);
-
-        useEffect(() => {
-          if (!isLoadingCookie && !user) {
-            router.replace("/signup-login");
-          }
-        }, [isLoadingCookie, user, router]);
-      
-        const [sessionData,setSessionData]=useState<sessionProps>({
-          context:"",
-          messages: [],
-          sessionType:"",
-          contextType:"",
-          sessionTitle:"",
-          fileUrl:"",
-        })
-
-        useEffect(() => {
-          if (sessionId && userId) {
-      
-            getSession(sessionId, userId).then((response:sessionProps) => {
-              setSessionData(response)
-              
-            });
-          }
-        }, [sessionId, userId]);
-
-        const parseContent = (content: string) => {
-          try {
-              // Remove Markdown formatting
-              const sanitizedContent = content.replace(/```mermaid|```/g, '').trim();
-              return sanitizedContent;
-          } catch (error) {
-              return null;
-          }
-      };
-        useEffect(()=>{
-           if(sessionData.context){
-            axios.post("api/flashCardExtract",{context:sessionData.context}).then(async (res)=>{
-              const finalData:string=await res.data.result;
-              console.log(typeof(finalData))
-               const parsed=JSON.parse(finalData);
-                  setResponse(parsed)
-                console.log("this is parsed "+parsed)
-              setIsLoading(false)
-      })
-           }
-
-        },[sessionData])
-      return (
-<div className="flex justify-center items-center min-h-screen bg-gray-900 border-2 border-white ">
-      {!isLoading?<>{JSON.stringify(response)}</>:<Spin size='large'></Spin>}
-    </div>    
-    )
+  useEffect(() => {
+    if (!isLoadingCookie && !user) {
+      router.replace("/signup-login");
     }
-    
-    export default page
+  }, [isLoadingCookie, user, router]);
 
+  const [sessionData, setSessionData] = useState<sessionProps>({
+    context: "",
+    messages: [],
+    sessionType: "",
+    contextType: "",
+    sessionTitle: "",
+    fileUrl: "",
+  });
+
+  useEffect(() => {
+    if (sessionId && userId) {
+      getSession(sessionId, userId).then((response: sessionProps) => {
+        setSessionData(response);
+      });
+    }
+  }, [sessionId, userId]);
+
+  const parseContent = (content: string) => {
+    try {
+      // Remove Markdown formatting
+      const sanitizedContent = content.replace(/```mermaid|```/g, '').trim();
+      return sanitizedContent;
+    } catch (error) {
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    if (sessionData.context) {
+      axios.post("api/flashCardExtract", { context: sessionData.context }).then(async (res) => {
+        const finalData: string = await res.data.result;
+        const parsed = JSON.parse(finalData);
+        setResponse(parsed);
+        setIsLoading(false);
+      });
+    }
+  }, [sessionData]);
+
+
+  const handleExit = () => {
+    setIsCardExiting(false);
+    setCurrentCardIndex((prevIndex) => (prevIndex + 1) % response.length);
+  };
+
+
+  const handleCardSelect = (index: number) => {
+    setCurrentCardIndex(index);
+  };
+
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-gray-900 border-2 border-white">
+      {!isLoading ? (
+        <>
+          <FlipCard content={isCardExiting ? '' : response[currentCardIndex]} resetToQuestion={queReset} onExit={handleExit} />
+          <button onClick={()=>{
+            if(currIndex>=0){
+            setIndex(currIndex-1)
+            setCurrentCardIndex(currIndex-1)
+            setQuesReset(true)}
+          }} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg">Prev</button>
+          <button onClick={()=>{
+            if(currIndex <= response.length){
+            setIndex(currIndex+1)
+            setCurrentCardIndex(currIndex+1)
+            setQuesReset(true)}
+          }} className="mt-4 px-4 py-2 bg-blue-600 text-white rounded-lg">Next</button>
+
+        </>
+      ) : (
+        <Spin size="large" />
+      )}
+    </div>
+  );
+};
+
+export default Page;
--- app/Components/flashcardComponents/FlipCard.tsx
+++ app/Components/flashcardComponents/FlipCard.tsx
@@ -1,20 +1,27 @@
+
 import React, { useState } from 'react';
+import { useEffect } from 'react';
 
-type Props = {};
+type Props = {
+  content: object;
+  onExit: () => void;
+  resetToQuestion: boolean;
+};
 
-const FlipCard = (props: Props) => {
-  const res = {
-    que: 'question',
-    ans: 'answer',
-  };
-  const [isFlipped, setIsFlipped] = useState(false);
-  const [width, setWidth] = useState('0'); // Initial width
+const FlipCard = ({ content, onExit ,resetToQuestion}: Props) => {
+
+  const [isExiting, setIsExiting] = useState(false);
+  const [isFlipped, setIsFlipped] = useState(true);
+  const [width, setWidth] = useState('16rem'); // Initial width
 
   const flip = () => {
     if (width === '0') {
       setWidth('16rem'); // Set width to the original width (16rem = w-64)
     } else {
       setWidth('0'); // Set width back to 0 before flipping
+      if(resetToQuestion){
+        setIsFlipped(!isFlipped)
+      }
       setTimeout(() => {
         setIsFlipped(!isFlipped);
         setWidth('16rem'); // Revert to original width after the transition
@@ -22,18 +29,22 @@ const FlipCard = (props: Props) => {
     }
   };
 
+  useEffect(()=>{
+    setIsFlipped(true)
+  },[content])
+
   return (
     <div
-      className="relative flex justify-center items-center h-64 bg-white transition-all duration-500"
-      style={{ width, perspective: '1000px' }}
+      className="relative flex justify-center items-center h-64 bg-transparent transition-all duration-500 mx-auto"
+      style={{ width, perspective: '1500px' }} // Increased perspective for more 3D effect
       onClick={flip}
     >
-      <div className={`relative w-full h-full transition-transform duration-500 transform ${isFlipped ? 'rotate-y-180' : ''}`}>
-        {isFlipped?<div className={`absolute w-full h-full bg-white flex justify-center items-center ${isFlipped ? 'backface-hidden transform rotate-y-180' : 'backface-hidden'}`}>
-          {res.que}
+      <div className={`relative w-full h-full transition-transform duration-500 transform-style-3d ${isFlipped ? 'rotate-y-180' : ''}`}>
+        {isFlipped?<div className="absolute w-full h-full bg-white backface-hidden flex justify-center items-center border border-gray-300 rounded-lg shadow-2xl text-xl font-bold p-4 transform rotate-y-0">
+          {content.question}
         </div>:
-        <div className={`absolute w-full h-full bg-[#800cb6] flex justify-center items-center ${isFlipped ? 'backface-hidden' : 'backface-hidden transform rotate-y-180'}`}>
-          {res.ans}
+        <div className="absolute w-full h-full overflow-hidden bg-purple-600 backface-hidden flex justify-center items-center border border-gray-300 rounded-lg shadow-2xl text-xl font-bold text-white p-4 transform rotate-y-180">
+          {content.answer}
         </div>}
       </div>
     </div>
